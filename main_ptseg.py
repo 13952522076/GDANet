06683793f60dc7e962d9a9a176d4f2d20ad2830a
13952522076 <xuma@my.unt.edu>
2021-05-22 23:27:49 -0400

    update

--- main_ptseg.py
+++ main_ptseg.py
@@ -382,7 +382,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='3D Shape Part Segmentation')
     parser.add_argument('--exp_name', type=str, default='GDANet', metavar='N',
                         help='Name of the experiment')
-    parser.add_argument('--batch_size', type=int, default=64, metavar='batch_size',
+    parser.add_argument('--batch_size', type=int, default=32, metavar='batch_size',
                         help='Size of batch)')
     parser.add_argument('--test_batch_size', type=int, default=32, metavar='batch_size',
                         help='Size of batch)')
